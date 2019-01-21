d636d1d1ee68ec0d8c7b384e8ba879ddbefaded2
Michael Tintiuc <contact@michaeltintiuc.com>
2019-01-21 02:14:10 +0200

    Initial commit. Modal controller example

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sandbox for testing [@ionic/vue](https://github.com/ionic-team/ionic/tree/master/vue/) docs
+
+## Installation
+
+```shell
+yarn install
+```
+
+###  Running local development server
+```
+yarn run serve
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <ion-page class="ion-page" main>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>@ionic/vue docs sandbox</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content padding>
+      <router-link to="modal">Modal controller</router-link>
+    </ion-content>
+  </ion-page>
+</template>
